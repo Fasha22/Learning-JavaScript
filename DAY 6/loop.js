ca4be880d0b8cdbc311c243474b958565497b650
Fasha22 <fashiefaith@gmail.com>
2024-06-04 14:32:36 +0300

    day 6

---
+++ DAY 6/loop.js
@@ -0,0 +1,25 @@
+// for (i = 0; i <= 5; i++) {
+//     console.log(`${i} * ${i} = ${i * i}`)
+// }
+
+// const numbers = [1, 2, 3, 4, 5]
+// const neArr = []
+
+// for(let i = 0; i < numbers.length; i++){
+//  neArr.push(numbers[i] **2)
+// }
+// console.log(neArr) 
+
+
+// let i = 0
+// while (i <= 5) {
+//   console.log(i)
+//   i++
+// }
+
+// let i = 0
+// do{
+//     console.log(i)
+//      i++
+// }
+// while()
